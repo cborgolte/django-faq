8692fd146654b1cd2d09482c87766a383dfb05ab
Christoph Borgolte <christoph.borgolte@googlemail.com>
2012-01-05 01:35:26 +0100

    Make the absolute_url configurable via settings.py

--- faq/models.py
+++ faq/models.py
@@ -1,9 +1,13 @@
 import datetime
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
 from django.contrib.auth.models import User
+from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
 from managers import QuestionManager
+
+
+faq_url_base = getattr(settings, 'FAQ_URL_BASE', 'faq')
 
 class Topic(models.Model):
     """
@@ -15,7 +19,7 @@ class Topic(models.Model):
         help_text=_('The order you would like the topic to be displayed.'))
 
     def get_absolute_url(self):
-        return '/faq/' + self.slug
+        return faq_url_base + self.slug
 
     class Meta:
         verbose_name = _("Topic")
@@ -34,20 +38,20 @@ class Question(models.Model):
         (INACTIVE,  _('Inactive')),
         (HEADER,    _('Group Header')),
     )
-    
+
     text = models.TextField(_('question'), help_text=_('The actual question itself.'))
     answer = models.TextField(_('answer'), blank=True, help_text=_('The answer text.'))
     topic = models.ForeignKey(Topic, verbose_name=_('topic'), related_name='questions')
     slug = models.SlugField(_('slug'), max_length=100)
     status = models.IntegerField(_('status'),
-        choices=STATUS_CHOICES, default=INACTIVE, 
+        choices=STATUS_CHOICES, default=INACTIVE,
         help_text=_("Only questions with their status set to 'Active' will be "
                     "displayed. Questions marked as 'Group Header' are treated "
                     "as such by views and templates that are set up to use them."))
-    
+
     protected = models.BooleanField(_('is protected'), default=False,
         help_text=_("Set true if this question is only visible by authenticated users."))
-        
+
     sort_order = models.IntegerField(_('sort order'), default=0,
         help_text=_('The order you would like the question to be displayed.'))
 
@@ -56,10 +60,10 @@ class Question(models.Model):
     created_by = models.ForeignKey(User, verbose_name=_('created by'),
         null=True, related_name="+")
     updated_by = models.ForeignKey(User, verbose_name=_('updated by'),
-        null=True, related_name="+")  
-    
+        null=True, related_name="+")
+
     objects = QuestionManager()
-    
+
     class Meta:
         verbose_name = _("Frequent asked question")
         verbose_name_plural = _("Frequently asked questions")
@@ -71,7 +75,7 @@ class Question(models.Model):
     def save(self, *args, **kwargs):
         # Set the date updated.
         self.updated_on = datetime.datetime.now()
-        
+
         # Create a unique slug, if needed.
         if not self.slug:
             suffix = 0
@@ -83,7 +87,7 @@ class Question(models.Model):
                     self.slug = potential
                 # We hit a conflicting slug; increment the suffix and try again.
                 suffix += 1
-        
+
         super(Question, self).save(*args, **kwargs)
 
     def is_header(self):
